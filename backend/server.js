db19a62c04e48b457c486c88e7f55874bad05513
elikaye <elianakaye13@gmail.com>
2024-11-08 08:00:03 -0300

    avances en mongo

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes'); // Importa las rutas
+const cors = require('cors');  // Importa cors
+const userRoutes = require('./routes/userRoutes');
 
-// Configura dotenv para leer el archivo .env
 dotenv.config();
 
-// Crea una instancia de Express
 const app = express();
 
-// Conectar a MongoDB
+// Configura CORS
+app.use(cors({
+    origin: 'http://localhost:3000',  // Cambia a la URL del frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos
+}));
+
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('Conectado a MongoDB'))
-.catch((err) => console.error('Error al conectar a MongoDB:', err));
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch((err) => console.error('Error al conectar a MongoDB:', err));
 
-// Middleware para analizar JSON
 app.use(express.json());
-
-// Conecta la ruta de usuarios
 app.use('/api/users', userRoutes);
 
-// Inicia el servidor en el puerto deseado
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
